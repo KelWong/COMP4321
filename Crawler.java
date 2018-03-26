7283d8195a96e1378fdc9ff5707fc79a39e4cea1
KelWong <wwhkelvin@gmail.com>
2018-03-26 23:00:09 +0800

    init

---
+++ Crawler.java
@@ -0,0 +1,8 @@
+// "every .java starts with  'public class [its name]'{}'"
+public class Crawler{
+    // = c++ that main()
+    public static void main (String args[]) {
+        // = cout << 
+        System.out.println("===========Comp4321: Search Engine=============");
+    }
+}
---
+++ OutputPlainText.java
@@ -0,0 +1,8 @@
+// "every .java starts with  'public class [its name]'{}'"
+public class OutputPlainText{
+    // = c++ that main()
+    public static void main (String args[]) {
+        // = c++ cout << ""
+        System.out.println("===========Comp4321: Search Engine=============");
+    }
+}
